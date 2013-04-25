5edda3e2739641b2484473ddfadc73208d2ce37d
tyranja <tyranja@cassiopeia.uberspace.de>
2013-04-25 22:04:54 +0200

    more tests

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    let(:output)  { double('output').as_null_object}
+    let(:game)    { Game.new(output)}
+
     describe "#start" do
       before(:each) do
         @output = double('output').as_null_object
@@ -10,12 +13,36 @@ module Codebreaker
 
       it "sends a welcome message" do
         @output.should_receive(:puts).with('Welcome to Codebreaker!')
-        @game.start
+        @game.start('1234')
       end
 
       it "prompts for the first guess" do
         @output.should_receive(:puts).with('Enter guess:')
-        @game.start
+        @game.start('1234')
+      end
+    end
+
+    describe '#guess' do
+      context "with no matches" do
+        it "sends a mark with ''" do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+      context "with one number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+      context "with 1 exact match" do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
       end
     end
   end
