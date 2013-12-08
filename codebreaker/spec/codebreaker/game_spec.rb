82ffa45f554e07d064fe1c1087777fef7e3c0e42
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-12-08 01:21:10 +0100

    write a working game.rb

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,23 +2,18 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
-    let(:output)  { double('output').as_null_object}
-    let(:game)    { Game.new(output)}
+    let(:output)  { double('output').as_null_object }
+    let(:game)    { Game.new(output) }
 
-    describe "#start" do
-      before(:each) do
-        @output = double('output').as_null_object
-        @game = Game.new(@output)
-      end
-
+    describe '#start' do
       it "sends a welcome message" do
-        @output.should_receive(:puts).with('Welcome to Codebreaker!')
-        @game.start('1234')
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+        game.start('1234')
       end
 
       it "prompts for the first guess" do
-        @output.should_receive(:puts).with('Enter guess:')
-        @game.start('1234')
+        output.should_receive(:puts).with('Enter guess:')
+        game.start('1234')
       end
     end
 
@@ -30,6 +25,7 @@ module Codebreaker
           game.guess('5555')
         end
       end
+
       context "with one number match" do
         it "sends a mark with '-'" do
           game.start('1234')
@@ -37,6 +33,7 @@ module Codebreaker
           game.guess('2555')
         end
       end
+
       context "with 1 exact match" do
         it "sends a mark with '+'" do
           game.start('1234')
@@ -44,6 +41,23 @@ module Codebreaker
           game.guess('1555')
         end
       end
+
+      context "with 2 numbers matches" do
+        it "sends a mark with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+
+      context "with 1 number match and 1 exact match (in that order" do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+-')
+          game.guess('2535')
+        end
+      end
+
     end
   end
 end
