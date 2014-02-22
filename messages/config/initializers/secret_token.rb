f607bfebd6a43b1b0219756fd75f2f1f359fe020
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-02-22 11:13:22 +0100

    add a new rails called messages for mocks

---
+++ messages/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = 'b5500bc0aeb23e7d32d6b3ed5bff691b98b282160eca7ccc81e65b911bbf89e17edc407776b3a9b0e86e6353f9106fd00b34176b914b1f18fd53f74934dbfb7f'
