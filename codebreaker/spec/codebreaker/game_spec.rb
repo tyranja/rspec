05b1208f47fa77e10a9bde1cbf03dd7c5a4f180f
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-02-22 11:12:52 +0100

    add codebreaker code

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
+require 'stringio'
 
 module Codebreaker
   describe Game do
-    let(:output)  { double('output').as_null_object }
+    let(:output)  {StringIO.new}
     let(:game)    { Game.new(output) }
 
     describe '#start' do
       it "sends a welcome message" do
         # expect(output.puts).to be == "Welcome to Codebreaker!"
-        output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start('1234')
+        output.seek(0)
+        expect(output.read).to include('Welcome to Codebreaker!')
       end
 
       it "prompts for the first guess" do
-        output.should_receive(:puts).with('Enter guess:')
+        # output.should_receive(:puts).with('Enter guess:')
         game.start('1234')
+        output.seek(0)
+        expect(output.read).to include('Enter guess:')
       end
     end
 
@@ -22,8 +26,8 @@ module Codebreaker
       context "with no matches" do
         it "sends a mark with ''" do
           game.start('1234')
-          output.should_receive(:puts).with('')
           game.guess('5555')
+          expect(output.read).to eq("")
         end
       end
     end
--- codebreaker/spec/codebreaker/marker_spec.rb
+++ codebreaker/spec/codebreaker/marker_spec.rb
@@ -6,27 +6,31 @@ module Codebreaker
       context "with no matches" do
         it "returns 0" do
           marker = Marker.new('1234', '5555')
-          marker.exact_match_count.should == 0
+          # marker.exact_match_count.should == 0
+          expect(marker.exact_match_count).to eq(0)
         end
       end
 
       context "with 1 exact match" do
         it "returns 1" do
           marker = Marker.new('1234', '1555')
-          marker.exact_match_count.should == 1
+          # marker.exact_match_count.should == 1
+          expect(marker.exact_match_count).to eq(1)
         end
       end
 
       context "with 1 number match" do
         it "returns 0" do
           marker = Marker.new('1234', '2555')
-          marker.exact_match_count.should == 0
+          # marker.exact_match_count.should == 0
+          expect(marker.exact_match_count).to eq(0)
         end
       end
       context "with 1 exact match and 1 number match" do
         it "returns 1" do
           marker = Marker.new('1234', '1525')
-          marker.exact_match_count.should == 1
+          # marker.exact_match_count.should == 1
+          expect(marker.exact_match_count).to eq(1)
         end
       end
     end
@@ -35,35 +39,40 @@ module Codebreaker
       context "with no matches" do
         it "returns 0" do
           marker = Marker.new('1234', '5555')
-          marker.number_match_count.should == 0
+          # marker.number_match_count.should == 0
+          expect(marker.number_match_count).to eq(0)
         end
       end
 
       context "with 1 exact match duplicated in guess" do
         it "returns 0" do
           marker = Marker.new('1234', '1155')
-          marker.number_match_count.should == 0
+          # marker.number_match_count.should == 0
+          expect(marker.number_match_count).to eq(0)
         end
       end
 
       context "with 1 number match" do
         it "returns 1" do
           marker = Marker.new('1234', '2555')
-          marker.number_match_count.should == 1
+          # marker.number_match_count.should == 1
+          expect(marker.number_match_count).to eq(1)
         end
       end
 
       context "with 1 exact match" do
         it "returns 0" do
           marker = Marker.new('1234', '1555')
-          marker.number_match_count.should == 0
+          # marker.number_match_count.should == 0
+          expect(marker.number_match_count).to eq(0)
         end
       end
 
       context "with 1 exact match and 1 number match" do
         it "returns 1" do
           marker = Marker.new('1234', '1525')
-          marker.number_match_count.should == 1
+          # marker.number_match_count.should == 1
+          expect(marker.number_match_count).to eq(1)
         end
       end
     end
